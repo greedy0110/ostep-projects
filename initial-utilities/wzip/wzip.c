4024aeece1c9b6e10f93bbdd8a9190e35946fe0e
greedy0110 <greedy0110@gmail.com>
2022-10-29 16:04:13 +0900

    done : aggregate_files_content

--- initial-utilities/wzip/wzip.c
+++ initial-utilities/wzip/wzip.c
@@ -79,14 +79,50 @@ void compress_file(const char* fn) {
     compress(source);
 }
 
+void aggregate_files_content(int num_files, const char* fn[], char **text) {
+    FILE *fps[num_files];
+    long fsizes[num_files];
+    long total_size = 0;
+    for (int i = 0; i<num_files; i++) {
+        fps[i] = fopen(fn[i], "r");
+        if (fps[i] == NULL) {
+            fprintf(stderr, "%s does not exist.", fn[i]);
+            exit(1);
+        }
+
+        if (fseek(fps[i], 0, SEEK_END) != 0) {
+            fprintf(stderr, "fseek failed (END)");
+            exit(1);
+        }
+        fsizes[i] = ftell(fps[i]);
+        total_size += fsizes[i];
+        if (fseek(fps[i], 0, SEEK_SET) != 0) {
+            fprintf(stderr, "fseek failed (SET)");
+            exit(1);
+        }
+    }
+
+    *text = (char*)malloc(total_size);
+    long offset = 0;
+
+    for (int i = 0; i<num_files; i++) {
+        fread(*text + offset, 1, fsizes[i], fps[i]);
+        fclose(fps[i]);
+        offset += fsizes[i];
+    }
+}
+
 int main(int argc, char const *argv[]) {
     if (argc == 1) {
         printf("wzip: file1 [file2 ...]\n");
         exit(1);
     }
 
-    for (int i=1; i<argc; i++) {
-        compress_file(argv[i]);
-    }
+    char *source;
+    //FIXME: split the array to argv[1..] to only pass the files list.
+    // Is it a good way to do it?
+    aggregate_files_content(argc-1, argv + 1, &source);
+    compress(source);
+
     return 0;
 }
