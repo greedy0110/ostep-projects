9e48f0a49b982d45cb16d0ba4ff4970360cd57c8
greedy0110 <greedy0110@gmail.com>
2022-11-11 09:40:42 +0900

    first running program /bin/ls

--- processes-shell/wish/main.c
+++ processes-shell/wish/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 const char* PROMPT = "wish> ";
 
@@ -26,6 +28,32 @@ int main(int argc, char const *argv[]) {
                 // argv[0] == command, argv[1..] == args
                 if (strcmp(argv[0], "exit") == 0) {
                     exit(0);
+                } else {
+                    // should execute another program.
+                    //TODO: path support
+                    if (access(argv[0], X_OK) == 0) {
+                        // argv[0] is an execuatable.
+                        pid_t rc = fork();
+                        if (rc == -1) {
+                            //TODO: print error
+                            printf("error occured");
+                        } else if (rc == 0) {
+                            // child
+                            rc = execv(argv[0], argv);
+                            if (rc == -1) {
+                                //TODO: print error
+                                printf("error occured");
+                            }
+                        } else {
+                            // parent
+                            // rc is the child process id.
+                            // parent should wait for the child to finish.
+                            if (waitpid(rc, NULL, 0) == -1) {
+                                //TODO: print error
+                                printf("error occured");
+                            }
+                        }
+                    }
                 }
             }
             printf("%s", PROMPT);
