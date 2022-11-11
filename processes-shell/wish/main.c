c330fe42825169131ccf6c54ad7679cee9e81ed5
greedy0110 <greedy0110@gmail.com>
2022-11-11 09:18:25 +0900

    first builtin command - exit

--- processes-shell/wish/main.c
+++ processes-shell/wish/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
 const char* PROMPT = "wish> ";
@@ -11,12 +12,24 @@ int main(int argc, char const *argv[]) {
         ssize_t linelen = 0;
         do {
             if (linelen != 0) {
+                line[linelen-1] = '\0'; // the return of getline is a new line. so earse it.
+
                 // parse
-                printf("echo : %s", line);
+                char *token;
+                char *argv[10]; // FIXME: 10?
+                int argc = 0;
+                while ((token = strsep(&line, " ")) != NULL) {
+                    argv[argc++] = token; 
+                }
+                argv[argc] = NULL;
+                
+                // argv[0] == command, argv[1..] == args
+                if (strcmp(argv[0], "exit") == 0) {
+                    exit(0);
+                }
             }
             printf("%s", PROMPT);
         } while ((linelen = getline(&line, &linecap, stdin)) > 0);
-
     } else if (argc == 2) {
         // batch mode
     } else {
