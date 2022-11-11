4d4c0ee30e18462c8569f490b812f379a3caf098
greedy0110 <greedy0110@gmail.com>
2022-11-11 21:38:23 +0900

    add batch mode support

--- processes-shell/code/main.c
+++ processes-shell/code/main.c
@@ -42,73 +42,88 @@ int execute(char *fullpath, char *argv[]) {
     return 1;
 }
 
+void parse_command_execute(char *raw_line) {
+    // parse
+    char *token;
+    char *argv[10]; // FIXME: 10?
+    int argc = 0;
+    while ((token = strsep(&raw_line, " ")) != NULL) {
+        argv[argc++] = token; 
+    }
+    argv[argc] = NULL;
+    
+    // argv[0] == command, argv[1..] == args
+
+    // builtin command
+    if (strcmp(argv[0], "exit") == 0) {
+        exit(0);
+    } else if (strcmp(argv[0], "cd") == 0) {
+        if (argc != 2) {
+            //TODO: print error
+            printf("cd error occured");
+            return;
+        }
+        if (chdir(argv[1]) != 0) {
+            //TODO: print error
+            printf("chdir error occured");
+        }
+    } else if (strcmp(argv[0], "path") == 0) {
+        int i;
+        for (i = 1; i < argc; i++) {
+            strcpy(paths[i-1], argv[i]);
+        } 
+        num_path = argc - 1;
+    } else {
+        // should execute another program.
+        // path support
+        char fullpath[1024];
+        int i;
+        for (i = 0; i<num_path; i++) {
+            strcpy(fullpath, paths[i]);
+            strcat(fullpath, "/");
+            strcat(fullpath, argv[0]);
+            if (execute(fullpath, argv) == 0) {
+                // execution success
+                break;
+            }
+        }
+    }
+}
+
 int main(int argc, char const *argv[]) {
 
+    FILE *in_fp;
     if (argc == 1) {
         // interact mode
-        char *line = NULL;
-        size_t linecap = 0;
-        ssize_t linelen = 0;
-        do {
-            if (linelen != 0) {
-                line[linelen-1] = '\0'; // the return of getline is a new line. so replace it.
-
-                // parse
-                char *token;
-                char *argv[10]; // FIXME: 10?
-                int argc = 0;
-                while ((token = strsep(&line, " ")) != NULL) {
-                    argv[argc++] = token; 
-                }
-                argv[argc] = NULL;
-                
-                // argv[0] == command, argv[1..] == args
-
-                // builtin command
-                if (strcmp(argv[0], "exit") == 0) {
-                    exit(0);
-                } else if (strcmp(argv[0], "cd") == 0) {
-                    if (argc != 2) {
-                        //TODO: print error
-                        printf("cd error occured");
-                        continue;
-                    }
-                    if (chdir(argv[1]) != 0) {
-                        //TODO: print error
-                        printf("chdir error occured");
-                    }
-                } else if (strcmp(argv[0], "path") == 0) {
-                    int i;
-                    for (i = 1; i < argc; i++) {
-                        strcpy(paths[i-1], argv[i]);
-                    } 
-                    num_path = argc - 1;
-                } else {
-                    // should execute another program.
-                    // path support
-                    char fullpath[1024];
-                    int i;
-                    for (i = 0; i<num_path; i++) {
-                        strcpy(fullpath, paths[i]);
-                        strcat(fullpath, "/");
-                        strcat(fullpath, argv[0]);
-                        if (execute(fullpath, argv) == 0) {
-                            // execution success
-                            break;
-                        }
-                    }
-                }
-            }
-            printf("%s", PROMPT);
-        } while ((linelen = getline(&line, &linecap, stdin)) > 0);
+        in_fp = stdin;
     } else if (argc == 2) {
         // batch mode
-        //TODO:
+        in_fp = fopen(argv[1], "r");
+        if (in_fp == NULL) {
+            //TODO: print error
+            printf("open error occured");
+            exit(1);
+        }
     } else {
         // wrong
-        //TODO:
+        //TODO: print error
+        printf("argc error occured");
         exit(1);
     }
+
+    char *line = NULL;
+    size_t linecap = 0;
+    ssize_t linelen = 0;
+    do {
+        if (linelen != 0) {
+            line[linelen-1] = '\0'; // the return of getline is a new line. so replace it.
+            parse_command_execute(line);
+        }
+        if (argc == 1) {
+            // interact mode only
+            printf("%s", PROMPT);
+        }
+    } while ((linelen = getline(&line, &linecap, in_fp)) > 0);
 
     return 0;
 }
